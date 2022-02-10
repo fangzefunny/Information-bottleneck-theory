8bc6fedd0f1b29731cbc10f86ebf601c691106b1
fangz5 <fangz5@rpi.edu>
2022-02-10 15:21:17 -0500

    init repo

--- main.py
+++ main.py
@@ -10,17 +10,61 @@ if not os.path.exists(f'{path}/checkpts'):
     os.mkdir(f'{path}/checkpts')
 
 #--------------------------------
+#        Data generation
+#--------------------------------
+
+# a function to an int to the input of the NN
+to_x = lambda x: list(map(int,list(f'{int(bin(x)[2:]):010d}')))
+
+def get_data( n_sample, groups, rng=None):
+    '''Generate artificial data
+
+    We need a experiemnt material to examine the data
+    Because I did not understand the example used in
+    Tishby's paper. I use the example in:
+    https://github.com/stevenliuyi/information-bottleneck
+
+    This is simple example, the input is a binary 
+    representation of number ranged in [0, 1023] in total
+    1024 possible input states. The states are divided into
+    16 groups, decided by their mod to 16. Each group output
+    a binary number either 0 or 1. 
+    '''
+    ## Decide the random generator 
+    rng = rng if rng is not None else np.random.RandomState(2022)
+
+    ## Get the input data X
+    x_int = rng.choice( 1023, size=n_sample)
+    x     = np.vstack( [ to_x(i) for i in x_int])
+    y0    = np.array([ groups[i%16] for i in x_int])
+    y1    = 1 - y0
+    y     = (np.vstack( [ y0, y1])).T
+
+    return x, y
+
+def get_train_test( n_train, n_test, seed=2022):
+    
+    ## Decide the random generator 
+    rng = np.random.RandomState( seed)
+
+    ## decide the group assignment
+    groups = rng.permutation( [0]*8+[1]*8)
+    x_train, y_train = get_data( n_train, groups, rng)
+    x_test , y_test  = get_data( n_test, groups, rng)
+
+    return x_train, y_train, x_test, y_test  
+
+#--------------------------------
 #        Hyperparameters
 #--------------------------------
 
 DefaultParams = { 
-                'Dims': [ 12, 10, 7, 5, 4, 3, 2],
-                'LR': 1e-3,
-                'MaxEpochs': 10000,
+                'LR': .1,
+                'MaxEpochs': 2000,
                 'Verbose': True,
                 'BatchSize': 32, 
                 } 
-eps_ = 1e-8
+eps_ = 1e-14
 
 #---------------------------------
 #    NeuralNetwork architecture
@@ -28,7 +72,7 @@ eps_ = 1e-8
 
 class NeuralNet(nn.Module):
 
-    def __init__( self, dims):
+    def __init__( self, dims, gpu=True):
         super().__init__()
         '''Network in the paper
 
@@ -38,6 +82,11 @@ class NeuralNet(nn.Module):
 
         Activation: tanh, sigmoid(last layer)
         '''
+        # assign device
+        if gpu and torch.cuda.is_available():
+            self.device = 'cuda'
+        else:
+            self.device = 'cpu'
         layers = []
         for i in range(len(dims)-1):
             layers.append( nn.Linear( dims[i], dims[i+1]))
@@ -46,32 +95,47 @@ class NeuralNet(nn.Module):
             else: 
                 layers.append( nn.Tanh())
         self.layers = nn.Sequential( *layers)
+        self.to(self.device)
 
     def forward( self, x):
         return self.layers(x)
 
+#---------------------------------
+#        Network Training 
+#---------------------------------
 
-def trainNN( train_data, **kwargs):
+def ACC( y_hat, y):
+    return (y_hat.argmax(dim=1) == y.argmax(dim=1)
+                ).float().mean().data.numpy()
+
+def trainNN( train_data, test_data, model, **kwargs):
     '''Train a neural network 
 
-        Architecture: fully connected 12-10-7-5-4-3-2
+        Architecture: fully connected 10-7-5-4-3-2
         Activation: tanh, sigmoid(last layer)
         Optimizer: SGD
         Loss fn: cross entropy
     '''
+    ## Hyperparameter
     HyperParams = DefaultParams
     for key in kwargs.keys():
         HyperParams[key] = kwargs[key]
-    # preprocess the data
-    x, y = train_data
-    n_batch = int( len(x))
-    x_tensor = x.type( torch.FloatTensor)
-    y_tensor = y.type( torch.FloatTensor)
-    _dataset = torch.utils.data.TensorDataset(x_tensor, y_tensor)
+
+    ## Preprocessing 
+    # preprocess the train data
+    x_train, y_train = train_data
+    n_batch = int( len(x_train))
+    x_train = torch.FloatTensor(x_train)
+    y_train = torch.FloatTensor(y_train)
+    _dataset = torch.utils.data.TensorDataset(x_train, y_train)
     _dataloader = torch.utils.data.DataLoader( _dataset, 
                 batch_size=HyperParams['BatchSize'], drop_last=True)
-    # init model
-    model = NeuralNet( HyperParams['Dims'])
+    # preprocess the test data 
+    x_test, y_test = test_data
+    x_test = torch.FloatTensor(x_test)
+    y_test = torch.FloatTensor(y_test)
+
+    ## Start training 
     # decide optimizer 
     optimizer = SGD( model.parameters(), lr=1e-2)   
     # init loss 
@@ -83,14 +147,11 @@ def trainNN( train_data, **kwargs):
     for epoch in range( HyperParams['MaxEpochs']):
 
         ## train each batch 
-        loss_ = 0        
+        loss_, train_acc = 0, 0        
         for _, (x_batch, y_batch) in enumerate(_dataloader):
 
-            # reshape the image
-            x = torch.FloatTensor( x_batch).view( 
-                x_batch.shape[0], -1)
-            # reconstruct x
-            y_hat =  model.forward( x)
+            # foward the model 
+            y_hat =  model.forward( x_batch.to(model.device))
             # calculate the loss 
             loss = BCE( y_hat, y_batch)
             # update
@@ -99,17 +160,28 @@ def trainNN( train_data, **kwargs):
             optimizer.step()
             # store the te losses for plotting 
             loss_ += loss.data.numpy() / n_batch
+            train_acc += ACC( y_hat, y_batch.to(model.device)) / n_batch
 
         # track training
         losses.append(loss_)
 
-        # save the model 
+        # Tracking the training condition 
         if (epoch%100 ==0) and HyperParams['Verbose']:
-            print( f'Epoch:{epoch}, Loss:{loss_}')
-            torch.save( model.state_dict(), f'{path}/checkpts/model-{epoch}.pkl')
+            y_test_hat =  model.forward( x_test.to(model.device))
+            test_acc   = ACC( y_test_hat, y_test.to(model.device))
+            print( f'Epoch:{epoch}: Train acc:{train_acc:.4f}-Test acc:{test_acc:.4f}')
+            #torch.save( model.state_dict(), f'{path}/checkpts/model-{epoch}.pkl')
 
 if __name__ == '__main__':
 
-    model = NeuralNet()
+    ## GET DATA
+    n_train, n_test = 50000, 10000 
+    data = get_train_test( n_train, n_test, seed=2022)
+    x_train, y_train, x_test, y_test = data
 
-    print(model)
+    ## train model
+    dims = [ 10, 7, 5, 4, 3, 2]
+    model = NeuralNet( dims, gpu=True)
+    trainNN( (x_train, y_train), (x_test, y_test), model, 
+                LR=.1)
+    
